d6e5651b3c5c7c77e4c5fa84b8d4f0c3655e03bf
Rafael Salimov <raphasalimov@gmail.com>
2023-01-20 11:45:37 +0400

    Connected to postgres

--- django-celery/core/settings.py
+++ django-celery/core/settings.py
@@ -68,8 +68,12 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": get_env_variable("DB_ENGINE"),
+        "NAME": get_env_variable("DB_NAME"),
+        "USER": get_env_variable("DB_USER"),
+        "PASSWORD": get_env_variable("DB_PASSWORD"),
+        "HOST": get_env_variable("DB_HOST"),
+        "PORT": get_env_variable("DB_PORT"),
     }
 }
 
