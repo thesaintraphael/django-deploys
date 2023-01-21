d9480c407a3bdf19d23e7418ee6bb307a629a243
Rafael Salimov <57571109+thesaintraphael@users.noreply.github.com>
2023-01-21 11:45:00 +0400

    Updated static settings

--- django-celery/core/settings.py
+++ django-celery/core/settings.py
@@ -109,12 +109,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_URL = "static/"
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
@@ -123,9 +117,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 STATIC_URL = "/static/"
 if DEBUG:
-    STATICFILES_DIRS = os.path.join(BASE_DIR, "media")
+    STATICFILES_DIRS = os.path.join(BASE_DIR, "static")
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, "media")
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = "/media/"
 
